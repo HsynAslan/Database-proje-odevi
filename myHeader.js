0c7fdb40cc64caac2d15ed24690adfe49ddd5666
Huseyin Aslan <btdhaslan@gmail.com>
2022-11-23 23:28:59 +0300

    menu bar worked

--- myHeader.js
+++ myHeader.js
@@ -1,5 +1,60 @@
 function showElement() {
+  document.getElementById("show_kadin_products").style.display = "none";
+  document.getElementById("show_cocuk_products").style.display = "none";
+  document.getElementById("show_guzellik_products").style.display = "none";
+  document.getElementById("show_erkek_products").style.display = "none";
   var x = document.getElementById("menu_bar_left");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function show_kadin() {
+  // document.getElementById() diğerlerini none yapmak lazım
+  document.getElementById("show_erkek_products").style.display = "none";
+  document.getElementById("show_cocuk_products").style.display = "none";
+  document.getElementById("show_guzellik_products").style.display = "none";
+  var x = document.getElementById("show_kadin_products");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function show_erkek() {
+  // document.getElementById() diğerlerini none yapmak lazım
+  document.getElementById("show_kadin_products").style.display = "none";
+  document.getElementById("show_cocuk_products").style.display = "none";
+  document.getElementById("show_guzellik_products").style.display = "none";
+  var x = document.getElementById("show_erkek_products");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function show_cocuk() {
+  // document.getElementById() diğerlerini none yapmak lazım
+  document.getElementById("show_kadin_products").style.display = "none";
+  document.getElementById("show_erkek_products").style.display = "none";
+  document.getElementById("show_guzellik_products").style.display = "none";
+  var x = document.getElementById("show_cocuk_products");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function show_makyaj() {
+  // document.getElementById() diğerlerini none yapmak lazım
+  document.getElementById("show_kadin_products").style.display = "none";
+  document.getElementById("show_erkek_products").style.display = "none";
+  document.getElementById("show_cocuk_products").style.display = "none";
+  var x = document.getElementById("show_guzellik_products");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
